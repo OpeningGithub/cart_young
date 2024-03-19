bce3b0412fceaf970bf982b3e020f07e61cc0048
openinggithub <19117305310@189.cn>
2024-03-19 09:30:33 +0800

    完成Table及Description的前端显示，Table to-do两个slot实现滑动框及按钮，Descriptions to-do实现货币数字的特定显示 * /src/views/cartTable.vue /src/plugins/element.js

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import {
-  Button, Table, TableColumn
+  Button, Table, TableColumn, Descriptions, DescriptionsItem
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Table)
 Vue.use(TableColumn)
+Vue.use(Descriptions)
+Vue.use(DescriptionsItem)
--- src/views/cartTable.vue
+++ src/views/cartTable.vue
@@ -1,29 +1,54 @@
 <template>
   <div>
-    <el-table
-      :data="tableData"
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
+    <el-table :data="tableData" border stripe style="width: 100%">
+      <el-table-column type="index" width="40"></el-table-column>
+      <el-table-column prop="name" label="商品名称" width="300"></el-table-column>
+      <el-table-column prop="price" label="商品单价" width="100"></el-table-column>
+      <el-table-column label="购买数量" width="100"></el-table-column>
+      <el-table-column label="操作" width="100"></el-table-column>
     </el-table>
+    <el-descriptions>
+      <el-descriptions-item label="总价">￥{{ totalPrice }}</el-descriptions-item>
+    </el-descriptions>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      tableData: [
+        {
+          id: 1,
+          name: 'AirPods',
+          price: 1288,
+          amount: 2
+        },
+        {
+          id: 2,
+          name: 'BeatX 入耳式耳机',
+          price: 1188,
+          amount: 5
+        },
+        {
+          id: 3,
+          name: 'Beats Solo3 Wireless 头戴式耳机',
+          price: 2288,
+          amount: 1
+        }
+      ],
+      totalPrice: 0
+    }
+  },
+  methods () {
+
+  },
+  created () {
+    for (let i = 0; i < this.tableData.length; ++i) {
+      this.totalPrice += this.tableData[i].amount * this.tableData[i].price
+    }
+  }
+}
 </script>
 
 <style></style>
